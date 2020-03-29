5bd5b9ee6863a8e86b2558cafaa051e65aaaebd5
rahulSharma-droid <62539696+rahulSharma-droid@users.noreply.github.com>
2020-03-29 18:09:41 +0530

    Update forecast.js

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,6 +1,7 @@
 const request = require('request')
 
 const forecast = (latitude, longitude, callback) => {
+    // Darksky url to get data in json format with specified latitude and longitude
     const url = 'https://api.darksky.net/forecast/983f58b1cf2af933931911508c386a2a/'+latitude+','+longitude+'?unit=si'
 
     request({ url, json: true }, (error, { body }) => {
